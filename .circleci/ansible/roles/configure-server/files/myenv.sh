d957afb977199e517b5488cf864366aaf68735a9
benjamin-andoh <benoo.andoh@gmail.com>
2023-03-24 22:59:34 +0000

    commit

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=adventsaas.ck0dwzfc16xf.eu-west-1.rds.amazonaws.com
+export TYPEORM_HOST=udapeople-db.cahlgcwy5b66.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
-export TYPEORM_USERNAME=benjamin
-export TYPEORM_PASSWORD=mypassword
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=W3yAk0TVCheMk9gOjC9a
 export TYPEORM_DATABASE=postgres
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
